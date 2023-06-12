0026dc8903e28090949ae298121090db48439734
Alexander <bykovskiyan@gmail.com>
2023-06-12 19:22:50 +0400

    Init

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+const converter = require("./modules/pr-236");
+
+const test1 = {
+    "participants": ["Mary", "Kate"],
+    "sports": ["football", "hockey"]
+}
+console.log(converter(test1.participants, test1.sports));
