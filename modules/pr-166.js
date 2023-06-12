8bde481fe816c53025b2a5e3321b2507a80b6fb1
Alexander <bykovskiyan@gmail.com>
2023-06-12 14:39:43 +0400

    Variant 2

--- modules/pr-166.js
+++ modules/pr-166.js
@@ -1,5 +1,16 @@
 module.exports = function (N, staff, K) {
-    staff.sort((a,b) => b-a);
+    let swapped;
+    do {
+        swapped = false;
+        staff.forEach((item, index) => {
+            if (item < staff[index + 1]) {
+                let temp = item;
+                staff[index] = staff[index + 1];
+                staff[index + 1] = temp;
+                swapped = true;
+            }
+        })
+    } while (swapped);
     let x = 0;
     for(let i=0; i<K; i++) {
         x+=staff[i];
