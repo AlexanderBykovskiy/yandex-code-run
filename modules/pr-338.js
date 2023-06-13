004dfdeba7474d4be2e15ccab8a2c802f714bafc
Alexander <bykovskiyan@gmail.com>
2023-06-13 21:39:24 +0400

    Variant 1

--- modules/pr-338.js
+++ modules/pr-338.js
@@ -1,5 +1,5 @@
 module.exports = function sumExcept ( nums, start, count ) {
-    let sum = 0;
+
     let len = nums.length;
 
     const validStart = Number.isInteger(start) && start > 0
@@ -7,33 +7,15 @@ module.exports = function sumExcept ( nums, start, count ) {
         : 0;
 
     const validCount = Number.isInteger(count) && count > 0
-        ? count + validStart > len ? len - validStart : count
+        ? validStart + count > len ? len - validStart : count
         : 0;
 
-    console.log('');
-    console.log('arr:', nums);
-    console.log('start:', start, 'count:', count, 'len:', len);
-    console.log('---', validStart, validStart + validCount);
+    // console.log('');
+    // console.log('arr:', nums);
+    // console.log('start:', start, 'count:', count, 'len:', len);
+    // console.log('---', validStart, validCount);
+    // nums.splice(validStart, validCount)
+    // console.log('splice:', nums);
 
-    const first = [];
-    if (validStart) {
-        for (let i = 0; i < validStart ; i++) {
-            if (Number.isInteger(nums[i])) {
-                sum += nums[ i ];
-                first.push(nums[ i ])
-            };
-        }
-    }
-    const second = [];
-    if (validCount) {
-        for (let i = validStart + validCount; i < len; i++) {
-            if (Number.isInteger(nums[i])) {
-                sum += nums[ i ];
-                second.push(nums[ i ])
-            };
-        }
-    }
-    console.log(first, second);
-
-    return sum ? sum : 0;
+    return nums.reduce((sum, item) => sum + (Number.isInteger(item) && item > 0 ? item : 0), 0);
 }
