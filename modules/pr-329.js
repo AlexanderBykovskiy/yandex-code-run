9ab2b9a507617e4f931307f2a7f94348f24d9223
Alexander <bykovskiyan@gmail.com>
2023-06-15 23:34:28 +0400

    Variant 1

--- modules/pr-329.js
+++ modules/pr-329.js
@@ -4,15 +4,33 @@ module.exports = function (inputStr) {
 
     const lib = ["-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."];
 
-    inputStr += " ";
+    inputStr = inputStr.trim() + " ";
     outputStr = "";
     const len = inputStr.length;
 
     let word = "";
 
     for (let i = 0; i < len; i++ ) {
+        if (inputStr[i] === ' ') {
 
+            if (word[0] === "T") word = word.slice(1, 6).split('').reverse().join('');
+
+            if (word[0] === "R") word = word.slice(1, 10).split('').filter((_, index) => index % 2 === 0).join('');
+
+            const findIndex = lib.findIndex(item => item === word);
+            if (findIndex >= 0)
+                outputStr+=findIndex
+
+            if (inputStr.slice(i, i + 3) === "   ") {
+                outputStr += " ";
+                i+=2;
+            }
+
+            word = "";
+        } else {
+            word+=inputStr[i];
+        }
     }
 
-    return inputStr;
+    return outputStr;
 }
