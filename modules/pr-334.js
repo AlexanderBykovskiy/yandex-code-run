c8d618143b041255a975df059bc282561bebb3af
Alexander <bykovskiyan@gmail.com>
2023-06-15 14:55:48 +0400

    Variant 1

---
+++ modules/pr-334.js
@@ -0,0 +1,10 @@
+/** @returns string */
+module.exports = function(inputString) {
+
+    const pattern = /ta['’]((So|Ko|Ta|Qa|Goo) \d+)/gi;
+
+    const result = Array.from(inputString.matchAll(pattern));
+
+    return result.length === 1 ? result[0][1].toLowerCase() : 0;
+
+}
