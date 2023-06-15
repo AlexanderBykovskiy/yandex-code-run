9810063e0f03791eb86e95afef49ccf6fcdfd07f
Alexander <bykovskiyan@gmail.com>
2023-06-15 13:56:57 +0400

    Temp commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hello World');
+const decoder = require("./modules/pr-329");
+
+const test1 = "T..... T---.. -.... .----   .---- R........-- T..... T.----   R.......... .---- T..... ---..   .---- R----...... R----...... ...--";
+console.log(decoder(test1));
---
+++ modules/pr-329.js
@@ -0,0 +1,18 @@
+module.exports = function (inputStr) {
+
+    if (inputStr.length < 5) return '';
+
+    const lib = ["-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."];
+
+    inputStr += " ";
+    outputStr = "";
+    const len = inputStr.length;
+
+    let word = "";
+
+    for (let i = 0; i < len; i++ ) {
+
+    }
+
+    return inputStr;
+}
