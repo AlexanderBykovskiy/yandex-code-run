c613ddffb5f0a7f922b167671252f73fbb2e2caa
Alexander <bykovskiyan@gmail.com>
2023-06-15 17:52:51 +0400

    Variant 3

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-console.log('Hello World');
+const { decode } = require("./modules/pr-320");
+
+
+const test1 = {"ops": ["Aa", [{ "from": "a", "to": "b" }]]}
+console.log(decode(test1.ops[0], test1.ops[1]));
---
+++ modules/pr-320.js
@@ -0,0 +1,28 @@
+/**
+ *
+ * @typedef Replace
+ *
+ * @property {string} from
+ * @property {string} to
+ */
+
+/**
+ *
+ * @param {string} message
+ * @param {Replace[]} replaces
+ * @returns {string}
+ */
+
+function decode(message, replaces) {
+
+    let phrase = "";
+    let decoded = ""
+
+    while (message.length) {
+        console.log(message[i])
+    }
+
+    return decoded
+}
+
+module.exports = { decode };
