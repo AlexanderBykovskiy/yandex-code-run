9e49cfad5b219fb4b9072f7352f456734b962b57
Alexander <bykovskiyan@gmail.com>
2023-06-15 12:01:35 +0400

    Variant 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,73 @@
-import { useState } from 'react'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <div>
-      +++
-    </div>
-  )
-}
 
-export default App
+/**
+ * onReserveFlight: (flight: string) => void; - Функция, в которую передается выбранный рейс. Вызвать один раз
+ * onAvailableFlights: (availableFlights: string[]) => void; - Функция, в которую передаются все доступные рейсы. Вызывать на каждом обновлении списка доступныъх рейсов
+ * getSuggestionsFromServer: (callback: (suggestions: string[]) => void) => void; - Функция, в коллбек которой приходит список доступных рейсов.
+ */
+const Form = ({
+                  onReserveFlight,
+                  onAvailableFlights,
+                  getSuggestionsFromServer,
+              }) => {
+
+    // Модифицировать код можно только внутри компонента Form
+
+    const [availableFlights, setAvailableFlights] = React.useState([]); // Например, ['Moscow', 'Paris', 'Milan']
+    const [pickedFlight, setPickedFlight] = React.useState(null);
+
+    React.useEffect(() => {
+        if (getSuggestionsFromServer) {
+            getSuggestionsFromServer((data) => {
+                setAvailableFlights(data);
+
+                // Не удаляйте эту строчку
+                setPickedFlight(data[0]);
+            })
+        }
+    }, []);
+
+    React.useEffect(() => {
+        if (onAvailableFlights) {
+            onAvailableFlights(availableFlights);
+        }
+    }, [availableFlights]);
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+
+        onReserveFlight(pickedFlight);
+    };
+
+    const onChooseFlight = (choosenFlight) => {
+        setPickedFlight(choosenFlight);
+    };
+
+    return (
+        <form onSubmit={(e) => onFormSubmit(e)}>
+            <ul>
+                Доступные рейсы:
+                {availableFlights.map((availableFlight, index) => (
+                    <li
+                        key={index}
+                        onClick={() => onChooseFlight(availableFlight)}
+                        style={{
+                            ...(availableFlight === pickedFlight && {
+                                border: "1px solid red"
+                            })
+                        }}
+                    >
+                        {availableFlight}
+                    </li>
+                ))}
+            </ul>
+
+            {/* Не меняйте аттрибут value у этого элемента */}
+            <input disabled={!pickedFlight} type="submit" value="Submit" />
+        </form>
+    );
+};
+
+export default Form
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Form from './App.jsx'
+
+const onReserveFlight = (flight) => console.log(flight)
+const onAvailableFlights = (availableFlights) => console.log(availableFlights)
+
+const getSuggestionsFromServer = (callback) => console.log(callback)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+    <Form
+        onReserveFlight ={onReserveFlight}
+        onAvailableFlights = {onAvailableFlights}
+        getSuggestionsFromServer = {getSuggestionsFromServer} // функция не работает
+    />
+
 )
