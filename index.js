984ae854034dcf025bb8337a7f8d1e559439041f
Alexander <bykovskiyan@gmail.com>
2023-06-17 23:30:43 +0400

    Variant 1

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const convert = require("./modules/pr-159");
 
 
-const test1 = "= head\n" +
-    "\n" +
-    "text ((https://ya.ru link)) text.\n" +
-    "\n" +
-    "* item\n" +
-    "* item"
-console.log("\n", convert(test1));
+const test1 = `= head '= head'
+
+text ((https://ya.ru link)) text.
+
+* item
+* item
+
+text ((https://google.com long link)) text.
+
+* item
+* item`;
+console.log("\nTest 1:", convert(test1));
--- modules/pr-159.js
+++ modules/pr-159.js
@@ -1,6 +1,57 @@
 module.exports = (str) => {
 
-    const result = "+++";
+    const header = /^= (.+)/i;
+    const listItem = /^\* (.+)/i;
+    const link = /\({2}(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_\+.~#?&\/\/=]*)) (.+)\){2}/i;
+
+    let result = "";
+    let strings = str.split("\n");
+
+    let list = "";
+    let isClosedUl = true;
+
+    if (strings.length === 0) return "";
+
+    let i = 0;
+    while (i < strings.length) {
+        const clearString = strings[i].replace(/\s+/gm,' ').replace(/'/gm,'"');
+        console.log(clearString);
+
+        // Find list
+        const findListItem = clearString.match(listItem);
+        if (findListItem) {
+            if (isClosedUl) {
+                result += "<ul>";
+                isClosedUl = false;
+            }
+            list += "<li>" + findListItem[1] + "</li>";
+            i++;
+            continue;
+        } else if (!isClosedUl && list.length) {
+            result += list + "</ul>";
+            list = "";
+            isClosedUl = true;
+        }
+
+        // Find header
+        const findHeader = clearString.match(header);
+        if (findHeader) {
+            result += "<h1>" + findHeader[1] + "</h1>";
+            i++;
+            continue;
+        }
+
+        // Find paragraph
+        if (clearString) {
+            result += "<p>" + clearString.replace(link, '<a href="$1">$4</a>') + "</p>";
+            i++;
+            continue;
+        }
+
+        i++;
+    }
+
+    if (!isClosedUl && list.length) result += list + "</ul>";
 
     return result;
 };
