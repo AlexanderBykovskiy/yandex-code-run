c1349ffd15dafe2d4e3ab44ad1a994a362483c2a
Alexander <bykovskiyan@gmail.com>
2023-06-18 17:16:11 +0400

    Variant 1

--- index.js
+++ index.js
@@ -5,16 +5,22 @@ const component = require('./component');
 
 describe('suite', function () {
     it('test1', function () {
-        // Some test 1
-        function () {} });
+        function () {
+            console.log("super");
+        };
     });
     it('test2', function () {
         // Some test 2
     });
     it.skip('test3', function () {
         // Some test 3
+        const lol = () => {
+            console.log('lalalal')
+        };
+        arr.map((i,j) => {
+            return i;
+        });
     });
-});
-`
+});`
 
 console.log("Test 1:", parser(test1));
--- modules/rp-256.js
+++ modules/rp-256.js
@@ -1,47 +1,33 @@
-module.exports = function(content /* текст тестового файла */) {
+module.exports = function(content) {
 
     const startBlock = /^[ ]*it(.skip)?\(/i;
-    const endBlock = /^[ ]*\}\);/i;
+    const endBlock = /^[ ]+\}\);/i;
+    const endAll = /^\}\);/i;
 
-    const blockList = [];
-    let block = "";
-    let isBlock = false;
-
-    content = content.replace(/\n$/,"").replace(/\n[ ]*\}\)$/i,"");
-
-    const contentArr = content.split('\n');
-    const len = contentArr.length;
-
-    for (let i=0; i<len; i++) {
-
-        if (!isBlock) {
-            const string = contentArr[i].match(startBlock);
-            if (string) {
-                isBlock = true;
-                block += contentArr[i] + "\n";
-            }
-        } else {
-            const string = contentArr[i].match(endBlock);
-            const nextStart = i + 1 < len ? contentArr[i + 1].match(startBlock) : true;
-            if (string && nextStart) {
-                isBlock = false;
-                block += contentArr[i];
-
-                blockList.push(block);
-
-                block = "";
-            } else {
-                if (i + 1 < len) block += contentArr[i] + "\n";
-            }
-        }
-
+    function wrapper (string) {
+        return "// Some imports and comments\nconst component = require('./component');\n\ndescribe('suite', function () {\n" + string + "\n});\n"
     }
 
-    console.log("+++++",block);
-    if (isBlock) blockList.push(block);
+    content = content.replace(/\n$/,"")
 
-    const result =  blockList.map(item => "// Some imports and comments\nconst component = require('./component');\n\ndescribe('suite', function () {\n" + item + "\n");
-    // console.log(result);
+    const arr = content.split("\n");
+    const len = arr.length
+    if (len < 2) return console.log('Wrong input data')
+
+    const blocks = [];
+    let blockIndStart = null
+
+    for (let i = 1; i < len; i++) {
+        if ( startBlock.test(arr[i]) || endAll.test(arr[i])) {
+            if (endBlock.test(arr[i - 1])) {
+                blocks.push([blockIndStart, i-1]);
+                blockIndStart = null;
+            }
+            blockIndStart = i;
+        }
+    }
+
+    const result = blocks.map (item => wrapper(arr.slice(item[0], item[1]+1).join("\n")));
     return result;
 }
 
