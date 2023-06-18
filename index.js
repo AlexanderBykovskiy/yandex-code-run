6f64d18b492b3904701d2610783b39e271804960
Alexander <bykovskiyan@gmail.com>
2023-06-18 23:04:53 +0400

    Init commit

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('Hello World');
+const {isRelativies} = require("./modules/pr-296");
+
+const test1 = {"genA":"AT","genB":"TA","level":1};
+console.log('test 1:', isRelativies(test1.genA, test1.genB, test1.level));
---
+++ modules/pr-296.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} genA
+ * @param {string} genB
+ * @param {number} level
+ * @returns {boolean}
+ */
+function isRelativies(genA, genB, level) {
+
+
+    return true;
+}
+
+exports.isRelativies = isRelativies;
