484557b645a7c181147dfcceee5452eba70108e0
Alexander <bykovskiyan@gmail.com>
2023-06-18 17:48:06 +0400

    Init commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+const { scan } = require("./modules/pr-285");
+
+
+const test1 = [
+    [1, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1],
+    [1, 1, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1],
+    [0, 1, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1]
+]
+console.log(scan(test1));
---
+++ modules/pr-285.js
@@ -0,0 +1,9 @@
+function scan (multiArray) {
+
+    const result = {ceil: 2, floor: 2, both: 1}
+
+    return result;
+}
+
+
+module.exports = { scan };
