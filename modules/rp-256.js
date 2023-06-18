ff257f6b3a64a027343373f86c70909a58ee92e3
Alexander <bykovskiyan@gmail.com>
2023-06-18 10:35:44 +0400

    Temp commit 2

--- modules/rp-256.js
+++ modules/rp-256.js
@@ -6,7 +6,9 @@ module.exports = function(content /* текст тестового файла */
 
     const result = [];
     matches.forEach(item => {
-        result.push("// Some imports and comments\nconst component = require('./component');\n\ndescribe('suite', function () {\n    " + item + "\n});")
+        const string ="// Some imports and comments\nconst component = require('./component');\n\ndescribe('suite', function () {\n    " + item + "\n});\n";
+        console.log(string);
+        result.push(string)
     });
 
     return result;
