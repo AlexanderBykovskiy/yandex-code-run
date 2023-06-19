3699f61b2846a7c61285f0fb61ac76313c7a244e
Alexander <bykovskiyan@gmail.com>
2023-06-19 21:27:09 +0400

    Init

--- solution.js
+++ solution.js
@@ -1,5 +1,14 @@
 const {getHashByData, fetchData} = require('./utils');
 
 module.exports = async function(urls, retryCount) {
-    console.log('+++');
+
+    const cb = (servHash) => console.log(servHash)
+
+
+    const response = await fetchData(urls[0])
+    console.log(response);
+    const hash = getHashByData(response.data, cb)
+    console.log(hash);
+
+    return [response.data];
 }
