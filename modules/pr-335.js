00c1695e3f6f761f967592002ec90c2f59628fae
Alexander <bykovskiyan@gmail.com>
2023-06-20 11:09:34 +0400

    Variant 3

--- modules/pr-335.js
+++ modules/pr-335.js
@@ -1,24 +1,21 @@
 module.exports = function (str) {
-
     const available = ["1111", "711", "7"];
+    const n = str.length;
 
     if (!(typeof str === 'string' || str instanceof String || Object.prototype.toString.call(str) === '[object String]') || str.length > 3e10 || str.length < 1) return false;
 
     if (!(/^[71]+$/.test(str))) return false;
-    const check = (pos, str) => {
 
-        if (pos === str.length) return true;
+    const dp = new Array(n + 1).fill(false);
+    dp[0] = true;
 
+    for (let i = 1; i <= n; i++) {
         for (const item of available) {
-            if (str.startsWith(item, pos)) {
-                const newPos = pos + item.length;
-                if (check(newPos, str)) return true;
+            if (item.length <= i && str.slice(i - item.length, i) === item) {
+                dp[i] = dp[i] || dp[i - item.length];
             }
         }
-
-        return false;
-
     }
 
-    return check(0, str);
-}
+    return dp[n];
+};
