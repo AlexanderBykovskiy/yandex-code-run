357c610a85ebab183904b0ad5dc4b0039115a983
Alexander <bykovskiyan@gmail.com>
2023-06-22 14:12:34 +0400

    Init

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const {isRelativies} = require("./modules/pr-296");
 
 const test1 = {"genA":"AT","genB":"TA","level":1};
-console.log('test 1:', isRelativies(test1.genA, test1.genB, test1.level));
+console.log('test 1 + :', isRelativies(test1.genA, test1.genB, test1.level));
+
+
+const test2 = {"genA":"ATTTGCGC","genB":"CGCGATTT","level":4};
+console.log('test 2 + :', isRelativies(test2.genA, test2.genB, test2.level));
--- modules/pr-296.js
+++ modules/pr-296.js
@@ -3,9 +3,14 @@
  * @param {string} genB
  * @param {number} level
  * @returns {boolean}
+ * A Аденин
+ * G Гуанин
+ * T Тимин
+ * C Цитозин
  */
 function isRelativies(genA, genB, level) {
 
+    if (!genA || !genB) return false;
 
     return true;
 }
