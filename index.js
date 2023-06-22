2de480dff053507ec7011ec995eb8a0192c37d74
Alexander <bykovskiyan@gmail.com>
2023-06-22 07:39:55 +0400

    Init

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello World');
+const timeCalculator = require("./modules/pr-218");
+
+
+const test1 = `+---0---+
+|       |
+| Y     |
+|       |
+2       |
+|       |
+|       |
+|       |
++---1---+`
+console.log("\nT 1\n", timeCalculator(test1));
+
+const test2 = `+----------------0---------------+
+|                                |
+|                                |
+|          U        D            |
+|     L                          |
+|              R                 |
+|           L                    |
+|  U                             1
+3        U    D                  |
+|         L              R       |
+|                                |
++----------------2---------------+`
+console.log("\nT 2\n", timeCalculator(test2));
---
+++ modules/pr-218.js
@@ -0,0 +1,9 @@
+module.exports = function (mapString) {
+
+    const rows = mapString.split("");
+    const rCount = mapString.length;
+
+    const timeInSec = Infinity;
+
+    return timeInSec; // первая секунда, в которую в кастрюле отсутствуют буквы
+}
