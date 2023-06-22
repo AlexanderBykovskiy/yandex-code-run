54de7812225e7b4776d3cd889bb27832cf38acdd
Alexander <bykovskiyan@gmail.com>
2023-06-22 18:17:04 +0400

    Variant 1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const {isRelativies} = require("./modules/pr-296");
 
-const test1 = {"genA":"AT","genB":"TA","level":1};
+const test1 = {"genA":"ATT","genB":"TAG","level":2};
 console.log('test 1 + :', isRelativies(test1.genA, test1.genB, test1.level));
 
 
--- modules/pr-296.js
+++ modules/pr-296.js
@@ -10,9 +10,33 @@
  */
 function isRelativies(genA, genB, level) {
 
+    level = String(level);
     if (!genA || !genB) return false;
+    if (genA === genB) return true;
 
-    return true;
+    function getParents(children) {
+        return [children.slice(0, children.length-1), children.slice(1, children.length)]
+    }
+
+    const parentsA = [genA];
+    let counter = 1;
+    for (let i = 0; i < level && i < genA.length; i++) {
+        for (let j = 0; j < counter; j++) {
+            //console.log("i", i, "j", counter + j - 1)
+            const nP = getParents(parentsA[counter + j - 1]);
+            //console.log("nP",nP)
+            parentsA.push(nP[0])
+            parentsA.push(nP[1])
+        }
+        counter*=2;
+    }
+
+    for(let i=0; i<parentsA.length; i++) {
+        const index = genB.indexOf(parentsA[i]);
+        if (index >=0 && index <= level) return true;
+    }
+
+    return false;
 }
 
 exports.isRelativies = isRelativies;
