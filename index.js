cc7fcaad0ea61580091164af141ddf4ad9ef574c
Alexander <bykovskiyan@gmail.com>
2023-06-23 19:37:47 +0400

    Variant 2

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const {isRelativies} = require("./modules/pr-296");
 
-const test1 = {genA:"ATGGC",genB:"TG",level:3};
-console.log('test 1:', isRelativies(test1.genA, test1.genB, test1.level));
+const test1 = {genA:"AT",genB:"TA",level:1};
+console.log('test 1: +', isRelativies(test1.genA, test1.genB, test1.level));
+console.log();
+
+const test2 = {genA:"AT",genB:"TA",level:2};
+console.log('test 2: +', isRelativies(test2.genA, test2.genB, test2.level));
+console.log();
+
+const test3 = {genA:"AT",genB:"TA",level:0};
+console.log('test 3: -', isRelativies(test3.genA, test3.genB, test3.level));
+console.log();
 
 
-const test2 = {genA:"A",genB:"T",level:3};
-//console.log(`test 2 + :${test2.genA} ${test2.genB}\n`, isRelativies(test2.genA, test2.genB, test2.level));
+const test4 = {genA:"ATGGC",genB:"TG",level:3};
+console.log(`test 4: +`, isRelativies(test4.genA, test4.genB, test4.level));
+console.log();
+
+
+const test5 = {genA:"ATGGC",genB:"TG",level:2};
+console.log('test 5: -', isRelativies(test5.genA, test5.genB, test5.level));
+console.log();
+
+
+const test6 = {genA:"ATGGC",genB:"TG",level:10};
+console.log(`test 6: +`, isRelativies(test6.genA, test6.genB, test6.level));
+console.log();
+
+
+const test7 = {genA:"AAA",genB:"AAA",level:0};
+console.log(`test 7: +`, isRelativies(test7.genA, test7.genB, test7.level));
+console.log();
+
+
+const test8 = {genA:"T",genB:"B",level:3};
+console.log('test 8:', isRelativies(test8.genA, test8.genB, test8.level));
+console.log();
--- modules/pr-296.js
+++ modules/pr-296.js
@@ -9,59 +9,35 @@
  * C Цитозин
  */
 function isRelativies(genA, genB, level) {
+    const [min, max] = genB.length > genA.length ? [genA, genB] : [genB, genA]
+    const stack = [{min, max, index: 0}]
 
-    if (genA === genB) {
-        console.log("input equal")
-        return true
-    }
-    if (!genA || !genB) {
-        console.log("input empty")
-        return false
-    }
-    if (Math.abs(genB.length - genA.length) > level) {
-        console.log("input small level")
+    if (max.length - min.length > level) {
         return false
     }
 
-    let long = genB.length >= genA.length ? genB : genA;
-    let short = genB.length < genA.length ? genB : genA;
-
-    let query = [{long: long, short: short, level: level}];
-
-    while (query.length && query[0].long.length > short.length) {
-        const pr1 = {long: query[0].long.slice(0,-1), short: short, level: query[0].level - 1}
-        const pr2 = {long: query[0].long.slice(1), short: short, level: query[0].level - 1}
-        if (pr1.long === pr1.short || pr2.long === pr2.short) {
-            console.log("hurray")
-            return true;
+    while (stack.length) {
+        const {min, max, index} = stack.pop()
+        if (index > level || min.length === 0) {
+            continue
         }
-        query.push(pr1);
-        query.push(pr2);
-        query = query.slice(1);
+
+        if (max.length > min.length) {
+            stack.push({max: max.slice(0, -1), min, index: index + 1})
+            stack.push({max: max.slice(1), min, index: index + 1})
+        } else {
+            if (min === max) {
+                return true
+            }
+            stack.push({max: max.slice(0, -1), min: min.slice(1), index: index + 1})
+            stack.push({max: max.slice(1), min: min.slice(1), index: index + 1})
+            stack.push({max: max.slice(0, -1), min: min.slice(0, -1), index: index + 1})
+            stack.push({max: max.slice(1), min: min.slice(0, -1), index: index + 1})
+        }
+
     }
-    //console.log("before ===",query)
 
-    while (query.length) {
-        if (query[0].long.length - 1 < 1 || query[0].level < 1) {
-            console.log("short parents")
-            return false;
-        }
-        const pr1 = {long: query[0].long.slice(0,-1), short: query[0].short.slice(0,-1), level: query[0].level - 1}
-        const pr2 = {long: query[0].long.slice(1), short: query[0].short.slice(0,-1), level: query[0].level - 1}
-        const pr3 = {long: query[0].long.slice(0,-1), short: query[0].short.slice(1), level: query[0].level - 1}
-        const pr4 = {long: query[0].long.slice(1), short: query[0].short.slice(1), level: query[0].level - 1}
-        if (pr1.long === pr1.short || pr2.long === pr2.short || pr3.long === pr3.short || pr4.long === pr4.short) {
-            console.log("hurray")
-            return true;
-        }
-        query.push(pr1);
-        query.push(pr2);
-        query.push(pr3);
-        query.push(pr4);
-        query = query.slice(1);
-    }
-    console.log("finished")
-    return false;
+    return false
 }
 
 exports.isRelativies = isRelativies;
