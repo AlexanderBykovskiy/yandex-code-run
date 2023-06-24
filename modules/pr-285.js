e44b5d1913c8811c200a6156eae4ad00c4515108
Alexander <bykovskiyan@gmail.com>
2023-06-24 23:25:19 +0400

    Variant 4

--- modules/pr-285.js
+++ modules/pr-285.js
@@ -16,21 +16,21 @@ const print = (multiArray, sailing = undefined, floor = undefined, index = undef
 
 function scan (multiArray) {
 
-    function lookAround (x,y) {
+    function lookAround (x,y, both = false) {
         const stack = [{x,y}];
         let ceil = false;
         let floor = false;
         while ( stack.length ) {
             const {x, y} = stack.pop();
             if (y === 0) ceil = true;
-            if (y === height-1) floor = true;
+            if (both && y === height-1) floor = true;
             multiArray[y][x] = 0;
             if (y+1 < height && multiArray[y+1][x] === 1) stack.push({x: x, y: y+1});
             if (y-1 >= 0 && multiArray[y-1][x] === 1) stack.push({x: x, y: y-1});
             if (x+1 < width && multiArray[y][x+1] === 1) stack.push({x: x+1, y: y});
             if (x-1 >= 0 && multiArray[y][x-1] === 1) stack.push({x: x-1, y: y});
         }
-        return {ceil, floor}
+        return both ? {ceil, floor} : {ceil}
     }
 
     let width = multiArray[0].length;
@@ -44,7 +44,7 @@ function scan (multiArray) {
 
     for (let i = 0; i < multiArray[0].length; i++) {
         if (multiArray[0][i] === 1) {
-            const {ceil, floor} = lookAround(i, 0);
+            const {ceil, floor} = lookAround(i, 0, true);
             if (ceil && floor) result.both+=1;
             if (ceil && !floor) result.ceil+=1;
         }
