2af01ccaf5f8b5a28097b9473a5560ec17dacc2d
Alexander <bykovskiyan@gmail.com>
2023-06-24 22:06:47 +0400

    Variant 2

--- modules/pr-285.js
+++ modules/pr-285.js
@@ -16,76 +16,58 @@ const print = (multiArray, sailing = undefined, floor = undefined, index = undef
 
 function scan (multiArray) {
 
+    function lookAround (stack) {
+        let ceil = false;
+        let floor = false;
+        while ( stack.length ) {
+            const {x, y} = stack.pop();
+            if (y === 0) ceil = true;
+            if (y === height-1) floor = true;
+            multiArray[y][x] = "*";
+            if (y+1 < height && multiArray[y+1][x] === 1) stack.push({x: x, y: y+1});
+            if (y-1 >= 0 && multiArray[y-1][x] === 1) stack.push({x: x, y: y-1});
+            if (x+1 < width && multiArray[y][x+1] === 1) stack.push({x: x+1, y: y});
+            if (x-1 >= 0 && multiArray[y][x-1] === 1) stack.push({x: x-1, y: y});
+        }
+        return {ceil, floor}
+    }
+
     let ceil = new Array(multiArray[0].length).fill(" ");
     let floor = new Array(multiArray[0].length).fill(" ");
 
     let width = multiArray[0].length;
     let height = multiArray.length;
 
-    const lookAroundT = (x, y) => {
-        //console.log("(", x, y, ")");
-        let end = false;
-        if (y-1 >= 0 && multiArray[y-1][x] === 1) {
-            //console.log("^");
-            multiArray[y-1][x] = "+";
-            // print(multiArray);
-            end = end || lookAroundT(x, y-1);
-        }
-        if (x-1 >= 0 && multiArray[y][x-1] === 1) {
-            //console.log("<");
-            multiArray[y][x-1] = "+";
-            // print(multiArray);
-            end = end || lookAroundT(x-1 , y);
-        }
-        if (x+1 < width && multiArray[y][x+1] === 1) {
-            //console.log(">");
-            multiArray[y][x+1] = "+";
-            // print(multiArray);
-            end = end || lookAroundT(x+1 , y);
-        }
-        if (y+1 < height && multiArray[y+1][x] === 1) {
-            //console.log("|");
-            multiArray[y+1][x] = "+";
-            // print(multiArray);
-            end = end || lookAroundT(x, y+1);
-        }
-
-        if (y+1 === height) {
-            end = true;
-        }
-
-        return end;
+    const result = {
+        ceil: 0,
+        floor: 0,
+        both: 0
     }
 
-    // top
-    for (let i=0; i<width; i++) {
-        let x = i;
-        let y = 0;
+    for (let i = 0; i < multiArray[0].length; i++) {
+        const stack = [];
+
         if (multiArray[0][i] === 1) {
-            ceil[i] = "c"
-            multiArray[y][x] = '+'
-            if (lookAroundT(x, y)) ceil[i] = "b";
+            stack.push({x: i, y: 0});
         }
+        const {ceil, floor} = lookAround(stack);
+        if (ceil && floor) result.both+=1;
+        if (ceil && !floor) result.ceil+=1;
     }
 
-    // bottom
-    for (let i=0; i<width; i++) {
-        let x = i;
-        let y = height-1;
+    for (let i = 0; i < multiArray[0].length; i++) {
+        const stack = [];
+
         if (multiArray[height-1][i] === 1) {
-            floor[i] = "f"
-            multiArray[y][x] = '+'
-            lookAroundT(x, y);
+            stack.push({x: i, y: height-1});
         }
+        const {ceil, floor} = lookAround(stack);
+        if (!ceil && floor) result.floor+=1;
     }
 
-    print(multiArray, ceil.join(""), floor.join(""));
+    // print(multiArray, ceil.join(""), floor.join(""));
 
-    return {
-        ceil: ceil.filter(item => item === "c").length,
-        floor: floor.filter(item => item === "f").length,
-        both: ceil.filter(item => item === "b").length
-    };
+    return result;
 }
 
 
