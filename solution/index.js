c2e356c02a200794c7ee2c3e0fa6e78e3984ea1f
Alexander <bykovskiyan@gmail.com>
2023-06-25 22:45:59 +0400

    Variant 1

--- solution/index.js
+++ solution/index.js
@@ -3,6 +3,7 @@ const TEXT_STYLES_MAPPER = {
     fontSize: (value) => `font-size: ${value}px;`,
     fontWeight: (value) => `font-weight: ${value};`,
     fontFamily: (value) => `font-family: ${value};`,
+    color: (value) => `color: ${value};`,
     textAlignHorizontal: (value) => `text-align: ${value.toLowerCase()};`,
     width: (value) => `width: ${value}px;`,
     paddingLeft: (value) => `padding-left: ${value}px;`,
@@ -11,6 +12,15 @@ const TEXT_STYLES_MAPPER = {
     paddingBottom: (value) => `padding-bottom: ${value}px;`,
     backgroundColor: (value) => `background-color: ${value};`,
     boxShadow: (value) => `box-shadow: ${value};`,
+    display: (value) => `display: ${value};`,
+    flexDirection: (value) => `flex-direction: ${value};`,
+    primaryAxisAlignItems: (value) => `justify-content: ${value};`,
+    counterAxisAlignItems: (value) => `align-items: ${value.toLowerCase()};`,
+    itemSpacing: (value) => `gap: ${value}px;`,
+    borderWidth: (value) => `border-width: ${value}px;`,
+    borderStyle: (value) => `border-style: ${value.toLowerCase()};`,
+    borderColor: (value) => `border-color: ${value};`,
+    boxHeight: (value) => `height: ${value}px;`,
 }
 
 
@@ -44,8 +54,32 @@ const getTextStyles = (node) => {
 
         if (key === "absoluteBoundingBox") {
             styleArr.push(TEXT_STYLES_MAPPER["width"](value.width));
+            styleArr.push(TEXT_STYLES_MAPPER["boxHeight"](value.height));
         } else if (key === "backgroundColor") {
             styleArr.push(TEXT_STYLES_MAPPER["backgroundColor"](`rgba(${Math.trunc(value.r * 255)}, ${Math.trunc(value.g * 255)}, ${Math.trunc(value.b * 255)}, ${value.a})`));
+        } else if (key === "layoutMode") {
+            if (value === "HORIZONTAL") {
+                styleArr.push(TEXT_STYLES_MAPPER["display"]("flex"));
+                styleArr.push(TEXT_STYLES_MAPPER["flexDirection"]("row"));
+            } else if (value === "VERTICAL") {
+                styleArr.push(TEXT_STYLES_MAPPER["display"]("flex"));
+                styleArr.push(TEXT_STYLES_MAPPER["flexDirection"]("column"));
+            }
+        } else if (key === "fills") {
+            if (value[0] && value[0]?.color?.r >= 0) {
+                styleArr.push(TEXT_STYLES_MAPPER[node.type === "RECTANGLE" ? "backgroundColor" : "color"](`rgba(${Math.trunc(value[0].color.r * 255)}, ${Math.trunc(value[0].color.g * 255)}, ${Math.trunc(value[0].color.b * 255)}, ${value[0].color.a})`));
+            }
+
+        } else if (key === "strokes") {
+            if (value[0] && value[0]?.color?.r >= 0 && value[0]?.type) {
+                styleArr.push(TEXT_STYLES_MAPPER["borderStyle"](value[0].type));
+                styleArr.push(TEXT_STYLES_MAPPER["borderColor"](`rgba(${Math.trunc(value[0].color.r * 255)}, ${Math.trunc(value[0].color.g * 255)}, ${Math.trunc(value[0].color.b * 255)}, ${value[0].color.a})`));
+                styleArr.push(TEXT_STYLES_MAPPER["borderWidth"](1));
+            }
+        } else if (key === "primaryAxisAlignItems") {
+            if (value === "SPACE_BETWEEN") {
+                styleArr.push(TEXT_STYLES_MAPPER["primaryAxisAlignItems"]("space-between"));
+            }
         } else if (TEXT_STYLES_MAPPER[key]) {
             styleArr.push(TEXT_STYLES_MAPPER[key](value));
         }
@@ -64,27 +98,29 @@ const PRIMITIVES = {
         });
     },
     FRAME: (node) => {
+        const parsedChildren = node.children.map(item => traverse(item)).join("");
         return buildBlock({
             type: 'div',
-            content: traverse(node.children[0]),
+            content: parsedChildren,
             className: node.type,
-            style: getTextStyles(node),
+            style: getTextStyles(node) + " box-sizing: border-box;",
         });
     },
     INSTANCE: (node) => {
+        const parsedChildren = node.children.map(item => traverse(item)).join("");
         return buildBlock({
             type: 'div',
-            content: "+++",
+            content: parsedChildren,
             className: node.type,
-            style: "+++",
+            style: getTextStyles(node) + " box-sizing: border-box;",
         });
     },
     RECTANGLE: (node) => {
         return buildBlock({
             type: 'div',
-            content: "+++",
+            content: "",
             className: node.type,
-            style: "+++",
+            style: getTextStyles(node) + " box-sizing: border-box;",
         });
     },
 };
@@ -94,7 +130,6 @@ const parse = (entry) => {
 };
 
 const traverse = (node) => {
-    console.log("******************************************")
     console.log(node.id, node.type, node.name)
     return PRIMITIVES[node.type](node);
 }
