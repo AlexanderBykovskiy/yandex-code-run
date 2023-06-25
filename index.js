44f4084e07a9b142c6b980cb1701f128d2afeb0c
Alexander <bykovskiyan@gmail.com>
2023-06-25 10:21:06 +0400

    Init

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log('Hello World');
+const {getLastCommonCommitMessage} = require("./modules/pr-186");
+
+
+console.log("add layout\n", getLastCommonCommitMessage([
+    {
+        id: '1',
+        message: 'initial commit',
+        timestamp: 1624010073113,
+    },
+    {
+        id: '2',
+        parents: ['1'],
+        message: 'add layout',
+        timestamp: 1624010082219,
+    },
+    {
+        id: '3',
+        parents: ['2'],
+        message: 'fix bugs',
+        timestamp: 1624010109039,
+        branches: ['master', 'bugfix']
+    },
+    {
+        id: '4',
+        parents: ['2'],
+        message: 'add link',
+        timestamp: 1624010179662,
+        branches: ['feature/link']
+    }
+], ['bugfix', 'feature/link']));
---
+++ modules/pr-186.js
@@ -0,0 +1,8 @@
+function getLastCommonCommitMessage(commits, branches) {
+
+
+
+    return {"error":true};
+};
+
+module.exports = {getLastCommonCommitMessage};
