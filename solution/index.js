7fef3e862ec01497c9b33ee4ac271feb08d4454d
Alexander <bykovskiyan@gmail.com>
2023-06-25 17:13:10 +0400

    Init

---
+++ solution/index.js
@@ -0,0 +1,7 @@
+/**
+ * Если нет идей, загляните в папку ./example
+ * В файле solution.js будет пример превращения компонента Text в HTML.
+ */
+module.exports = function(json) {
+    return "<div>Hello</div>";
+}
