0c4de366a919aa45c49f26ca9b9783e3d251d77c
Alexander <bykovskiyan@gmail.com>
2023-06-25 16:03:40 +0400

    Variant 1

--- modules/pr-265.js
+++ modules/pr-265.js
@@ -18,18 +18,39 @@ module.exports = function (field, moves) {
     field.forEach(item => {
         fieldArr.push(item.split(""));
     })
-    const movesArr = moves.split(" ");
 
     fieldArr[0][0] = "-";
     fieldArr[0][1] = "-";
     fieldArr[0][2] = "-";
 
+    const temp = moves.split(" ");
+    const movesArr = [];
+    for (let i = 0; i < temp.length; i+=2) {
+        movesArr.push([temp[i], temp[i+1]])
+    }
+    //console.log(movesArr)
+
     let head = {r: 0, c: 2};
     let size = 3;
 
-    print(fieldArr);
+    function go(way) {
+        while(way[1]) {
+            if (way[0] === "R") head.c += 1;
+            if (way[0] === "L") head.c -= 1;
+            if (way[0] === "D") head.r += 1;
+            if (way[0] === "U") head.r -= 1;
+            if (food.includes(fieldArr[head.r][head.c])) {
+                size++;
+            }
+            fieldArr[head.r][head.c] = "*";
+            way[1]--;
+        }
+    }
 
-    console.log(movesArr)
+    movesArr.forEach(item => go(item));
 
-    return [[0, 0], 0]; // [x, y] - координаты головы змейки, N - размер змейки
+    //print(fieldArr);
+
+
+    return [[head.r, head.c], size]; // [x, y] - координаты головы змейки, N - размер змейки
 }
