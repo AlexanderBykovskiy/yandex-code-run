e4ba5a4a95f1eb84c8ac2f46841c424822d99bb1
Alexander <bykovskiyan@gmail.com>
2023-06-25 08:31:24 +0400

    Variant 5 Accepted

--- modules/pr-285.js
+++ modules/pr-285.js
@@ -1,21 +1,15 @@
 function scan (multiArray) {
 
-    let width = multiArray[0].length;
-    let height = multiArray.length;
-
     const result = {
         ceil: 0,
         floor: 0,
         both: 0
     }
 
-    if (!width || !height) return result;
-
     const stack = [];
 
-    for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-            console.log(multiArray[y][x])
+    for (let y = 0; y < multiArray.length; y++) {
+        for (let x = 0; x < multiArray[0].length; x++) {
             if (multiArray[y][x] === 1) {
                 stack.push({x,y})
                 let ceil = false;
@@ -23,16 +17,20 @@ function scan (multiArray) {
                 while ( stack.length ) {
                     const {x, y} = stack.pop();
                     if (y === 0) ceil = true;
-                    if (y === height-1) floor = true;
+                    if (y === multiArray.length-1) floor = true;
                     multiArray[y][x] = "*";
-                    if (y+1 < height && multiArray[y+1][x] === 1) stack.push({x: x, y: y+1});
+                    if (y+1 < multiArray.length && multiArray[y+1][x] === 1) stack.push({x: x, y: y+1});
                     if (y-1 >= 0 && multiArray[y-1][x] === 1) stack.push({x: x, y: y-1});
-                    if (x+1 < width && multiArray[y][x+1] === 1) stack.push({x: x+1, y: y});
-                    if (x-1 >= 0 && multiArray[y][x-1] === 1) stack.push({x: x-1, y: y});
+                    if (multiArray[y][x+1] === 1) stack.push({x: x+1, y: y});
+                    if (multiArray[y][x-1] === 1) stack.push({x: x-1, y: y});
                 }
-            if (ceil && floor) result.both+=1;
-            if (ceil && !floor) result.ceil+=1;
-            if (!ceil && floor) result.floor+=1;
+                if (ceil && floor) {
+                    result.both += 1
+                } else {
+                    if (ceil) result.ceil+=1;
+                    if (floor) result.floor+=1;
+                }
+
             }
         }
     }
