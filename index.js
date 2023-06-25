814c2773af2f7087bab9e0918816b9302a2ed8dc
Alexander <bykovskiyan@gmail.com>
2023-06-25 07:20:36 +0400

    init

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
+const parser = require("./modules/pr-192");
+
+
 console.log('Hello World');
---
+++ modules/pr-192.js
@@ -0,0 +1,9 @@
+/**
+ * @param {Band|Genre} data - ссылка на группу или жанр,
+ * из которой нужно восстановить все возможные данные
+ * @return {string}
+ */
+module.exports = function (data) {
+    // ваш код
+    return '…';
+}
