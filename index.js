2cbdf749ff93a6562427bf7846250c8ee87c0eb3
Alexander <bykovskiyan@gmail.com>
2023-06-26 17:14:20 +0400

    Variant 1

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-console.log('Hello World');
+const express = require('express');
+const { BEEP_CODES } = require('@yandex-blitz/phone');
+
+const createApp = ({ phone }) => {
+    const app = express();
+
+    // звонит по номеру записанному в "быстром наборе" под цифрой digit
+    app.get('/speeddial/:digit', async (req, res) => {
+        try {
+            const value = await phone.getData();
+            const speeddialDict = JSON.parse(value);
+
+            await phone.connect();
+            phone.dial(speeddialDict[req.params.digit]);
+
+            res.sendStatus(200);
+        } catch (error) {
+            phone.beep(BEEP_CODES.ERROR);
+            res.sendStatus(500);
+        }
+    });
+
+    // записывает в "быстрый набор" под цифру digit номер phonenumber
+    app.post('/speeddial/:digit/:phonenumber', async (req, res) => {
+        try {
+            const value = await phone.getData();
+            const speeddialDict = JSON.parse(value);
+            speeddialDict[req.params.digit] = Number(req.params.phonenumber);
+
+            await phone.setData(JSON.stringify(speeddialDict));
+            phone.beep(BEEP_CODES.SUCCESS);
+
+            res.sendStatus(200);
+        } catch (error) {
+            phone.beep(BEEP_CODES.ERROR);
+            res.sendStatus(500);
+        }
+    });
+
+    return app;
+};
+
+exports.createApp = createApp;
