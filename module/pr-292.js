7a2751cc04a7b77eb71b6c7aa4fbb5f027bbdb0f
Alexander <bykovskiyan@gmail.com>
2023-06-30 15:40:58 +0400

    temp commit

--- module/pr-292.js
+++ module/pr-292.js
@@ -1,16 +1,9 @@
-const printLayer = (arr, message = undefined) => {
-    if(message) console.log(message, "-")
-    arr.forEach(item => console.log(item.join("")))
-    console.log("-")
-}
-
 module.exports = function solveCaptcha(captcha) {
 
     const captchaArr = captcha.trim()
         .split("\n").map(item => item.trim());
-    //printLayer (captchaArr)
-    console.log(captchaArr)
-    console.log("x:", captchaArr[0].length, "y:", captchaArr.length)
+    //console.log(captchaArr)
+    //console.log("x:", captchaArr[0].length, "y:", captchaArr.length)
 
     function getSCount (point, size) {
         let signCount = 0;
@@ -30,13 +23,13 @@ module.exports = function solveCaptcha(captcha) {
 
     if (fullS % signCount !== 0) return [];
     const partS = fullS / signCount;
-    console.log("S:", partS)
+    //console.log("S:", partS)
 
     let rectangles = [];
     for (let i = captchaArr[0].length; i > 0; i--) {
         if (partS % i === 0 && (partS / i) <= captchaArr.length) rectangles.push([i, partS / i]);
     }
-    console.log("rectangles:", rectangles)
+    //console.log("rectangles:", rectangles)
 
 // #####################################################################################################################
 
@@ -59,10 +52,10 @@ module.exports = function solveCaptcha(captcha) {
 
         const newLayer = Object.assign([], board[board.length-1]);
 
-        console.log("\nGET LAYER")
-        console.log("pos", pos, "size", size)
+        //console.log("\nGET LAYER")
+        //console.log("pos", pos, "size", size)
 
-        console.log("end x", pos[0] + size[0] , "need", newLayer[0].length, "       end y", pos[1] + size[1] , "need", newLayer.length)
+        //console.log("end x", pos[0] + size[0] , "need", newLayer[0].length, "       end y", pos[1] + size[1] , "need", newLayer.length)
 
         if (pos[1] + size[1] > newLayer.length || pos[0] + size[0] > newLayer[0].length) return null;
 
@@ -73,27 +66,34 @@ module.exports = function solveCaptcha(captcha) {
             }
         }
 
-        console.log("S count", sCount)
+        //console.log("S count", sCount)
 
         if (sCount !== 1) return null;
 
-
         for (let y = pos[1]; y < pos[1] + size[1]; y++) {
-            newLayer[y] = newLayer[y].slice(0, pos[0]) + "#".repeat(pos[0] + size[0]) + newLayer[y].slice(pos[0] + size[0], newLayer[y].length);
-            // for (let x = pos[0]; x < pos[0] + size[0]; x++) {
-            //     if (newLayer[y][x] === "S") {
-            //         newLayer[y][x] = "*";
-            //     } else {
-            //         newLayer[y][x] = "#";
-            //     }
-            // }
+            newLayer[y] = newLayer[y].slice(0, pos[0]) + "#".repeat(size[0]) + newLayer[y].slice(pos[0] + size[0], newLayer[y].length);
+            //newSolution.push(newLayer[y].slice(pos[0], size[0]));
         }
 
-        console.log("\n---ACCEPT---")
-        console.log(newLayer)
-        console.log("\n\n")
+        //console.log("\n---ACCEPT---")
+        //console.log(newLayer)
+        //console.log("\n\n")
 
         return newLayer;
+    }
+
+    const newSolution = (pos, size) => {
+
+        const newLayer = Object.assign([], board[board.length-1]);
+        const sol = [];
+
+
+        if (pos[1] + size[1] > newLayer.length || pos[0] + size[0] > newLayer[0].length) return null;
+
+        for (let y = pos[1]; y < pos[1] + size[1]; y++) {
+            sol.push(newLayer[y].slice(pos[0], pos[0] + size[0]));
+        }
+        return sol.length ? sol : null;
     }
 
 
@@ -103,37 +103,48 @@ module.exports = function solveCaptcha(captcha) {
 
         const pos = getEmptyPos(board);
         //if (!pos) return board; // ********
-        console.log("\n-----------------------------\nPOS", pos, "level", num);
-        console.log(board[board.length-1])
+        //console.log("\n-----------------------------\nPOS", pos, "level", num);
+        //console.log(board[board.length-1])
 
         for (let i = 0; i < rectangles.length; i++) {
 
             const size = rectangles[i];
 
             const layer = getLayer(pos, size);
-            if (layer) console.log("* layer ok *")
+            const newSol = newSolution(pos, size);
+
+            //if (layer) console.log("* layer ok *")
 
             if (layer) {
 
                 board.push(Object.assign([],layer));
+                solution.push(Object.assign([],newSol));
 
                 const res = nextLayout(num - 1);
+
 
                 if (res) return res;
 
                 board.pop();
+                solution.pop();
             }
 
         }
 
-        console.log("==== all figuries, last is ====")
-        board.map(item => console.log(item))
+        //console.log("==== all figuries, last is ====")
+        //board.map(item => console.log(item))
     }
 
     const board = [Object.assign([], captchaArr)];
+    const solution = [];
 
     const res = nextLayout(signCount)
 
-    return "answer";
+    //console.log("res", res)
+    const result = solution.map(item => item.join("\n"));
+    //console.log("sol", solution.map(item => item.join("\n")))
+
+
+    return result;
 
 }
