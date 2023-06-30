6f6eea1df68318295a0a96612c1fc37a2c86ed75
Alexander <bykovskiyan@gmail.com>
2023-06-30 07:34:03 +0400

    Init

--- out/main.js
+++ out/main.js
@@ -1,7 +1,5 @@
 // Не забудьте перед отправкой изменить в module.exports = function(html, css) {
-export default function(html, css) {
-
-    console.log(html)
-
+export default function (html, css) {
+    console.log(html);
     return html;
 }
