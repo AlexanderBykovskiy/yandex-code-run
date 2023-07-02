c71f55ee6a787223fe7835e90273325f05536593
Alexander <bykovskiyan@gmail.com>
2023-07-02 13:09:39 +0400

    Temp

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const robot = require('./modules/pr-179');
 
-const test1 = [
+/*const test1 = [
     {
         id: '#1',
         created: 1536077100,
@@ -22,4 +22,39 @@ const test1 = [
         files: ['index.spec.js', 'index.spec.ts', 'index.ts']
     }
 ]
-console.log(robot(test1));
+
+console.log(robot(test1), [
+    "#1",
+    "#2",
+    "#4"
+]);*/
+
+const test2 = [
+    {
+        "id": "1",
+        "created": 1538179200,
+        "files": [ "a", "b", "c", "d" ]
+    },
+    {
+        "id": "2",
+        "created": 1538189200,
+        "files": [ "a", "x" ]
+    },
+    {
+        "id": "3",
+        "created": 1538199200,
+        "files": [ "b", "g" ]
+    },
+    {
+        "id": "4",
+        "created": 1538209200,
+        "files": [ "c",  "f" ]
+    },
+    {
+        "id": "5",
+        "created": 1538219200,
+        "files": [ "d", "w" ]
+    }
+]
+
+console.log(robot(test2), ['2', '3', '4', '5']);
--- modules/pr-179.js
+++ modules/pr-179.js
@@ -4,9 +4,116 @@
  */
 module.exports = function (pullRequests) {
 
-    return [
-        "#1",
-        "#2",
-        "#4"
-    ]
+    const conflictMatrix = new Uint8Array(pullRequests.length ** 2);
+    //console.log("conflictMatrix", conflictMatrix)
+    const prToIndex = new WeakMap();
+    //console.log("prToIndex", prToIndex)
+
+    for (let i = 0; i < pullRequests.length; i++) {
+        const pr1 = pullRequests[i];
+        prToIndex.set(pr1, i);
+        //console.log("prToIndex",prToIndex)
+        conflictMatrix[i * pullRequests.length + i] = 0;
+
+        for (let j = i + 1; j < pullRequests.length; j++) {
+            const pr2 = pullRequests[j];
+            //console.log("pullRequests[j]", j, pullRequests[j])
+            const res = conflicts(pr1.files, pr2.files);
+            conflictMatrix[i * pullRequests.length + j] = res;
+            conflictMatrix[j * pullRequests.length + i] = res;
+            // conflictMatrix[i * pullRequests.length + j] = conflictMatrix[j * pullRequests.length + i] = conflicts(pr1.files, pr2.files);
+        }
+    }
+
+    console.log('conflictMatrix')
+    console.log(conflictMatrix.length)
+    for (let i = 0; i < pullRequests.length; i++) {
+        console.log(conflictMatrix.slice(i*pullRequests.length, i*pullRequests.length + pullRequests.length))
+    }
+
+    function doPRsConflict(pr1, pr2) {
+        const i = prToIndex.get(pr1);
+        const j = prToIndex.get(pr2);
+        return conflictMatrix[i * pullRequests.length + j] === 1;
+    }
+
+
+    function getNonConflictingPRs (prsSet, mergedPrs) {
+        const result = [];
+        const prsToTest = [...prsSet, ...mergedPrs];
+        prsSet.forEach((pr) => {
+            if (!doPRsConflict(pr, prsToTest)) {
+                result.push(pr);
+            }
+        });
+        return result;
+    }
+
+    console.log("+++",getNonConflictingPRs(pullRequests, ["2", "3"]))
+
+
+    function conflicts(a, b) {
+        let i = 0;
+        let j = 0;
+
+        while (i < a.length && j < b.length) {
+            if (a[i] === b[j]) {
+                return true;
+            } else if (a[i] > b[j]) {
+                j++;
+            } else {
+                i++;
+            }
+        }
+
+        return false;
+    }
+
+    function mergeAllPrs (pullRequests) {
+        let i = 0;
+        const mergedFiles = [];
+        const mergedPrs = [];
+        while (i < pullRequests.length) {
+            const pr = pullRequests[i];
+            if (!conflicts(mergedFiles, pr.files)) {
+                mergedPrs.push(pr);
+                mergedFiles.push(...pr.files);
+            }
+            i++;
+        }
+        return mergedPrs.map(x => x.id);
+    }
+
+    const fullSearch = (prsSet, mergedPrs = [], mergedFilesCount = 0) => {
+
+        //допиши код
+
+        // выбираем реквесты, которые не конфликтуют ни с одним из смерженных и оставшихся
+        // их можно смержить, и конфликтов не будет
+        const safeToMergePRs = getNonConflictingPRs(prsSet, mergedPrs);
+        mergedPrs = mergedPrs.concat(safeToMergePRs);
+        safeToMergePRs.forEach((pr) => {
+            prsSet.delete(pr);
+            mergedFilesCount += pr.files.length;
+        });
+
+        const pr = prsSet.values().next().value;
+
+        //допиши код
+
+    }
+
+
+
+
+    let result = [];
+
+    if (!pullRequests.length) return [];
+
+    if (pullRequests.length === 1) return [pullRequests[0].id];
+
+
+    //допиши код
+
+    return result;
 }
