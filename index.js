b98e55262a3e672c12015a7b4af5e1084d693fa4
Alexander <bykovskiyan@gmail.com>
2023-07-02 11:26:59 +0400

    Init

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Hello World');
+const robot = require('./modules/pr-179');
+
+const test1 = [
+    {
+        id: '#1',
+        created: 1536077100,
+        files: ['.gitignore', 'README.md']
+    },
+    {
+        id: '#2',
+        created: 1536077700,
+        files: ['index.js', 'package-lock.json', 'package.json']
+    },
+    {
+        id: '#3',
+        created: 1536077800,
+        files: ['.pnp.js', 'package-lock.json', 'yarn.lock']
+    },
+    {
+        id: '#4',
+        created: 1536077900,
+        files: ['index.spec.js', 'index.spec.ts', 'index.ts']
+    }
+]
+console.log(robot(test1));
---
+++ modules/pr-179.js
@@ -0,0 +1,12 @@
+/**
+ * @param {PullRequest[]} pullRequests массив PR, отсортированных по времени создания
+ * @returns {string[]} идентификаторы реквестов в порядке мёржа
+ */
+module.exports = function (pullRequests) {
+
+    return [
+        "#1",
+        "#2",
+        "#4"
+    ]
+}
