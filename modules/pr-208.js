fcb2abd811f86bd54af9ba65e4f208c681645639
Alexander <bykovskiyan@gmail.com>
2023-07-04 11:39:18 +0400

    Temp

--- modules/pr-208.js
+++ modules/pr-208.js
@@ -1,5 +1,11 @@
 function getLastCompatibleDependencies(data, packageA, packageB) {
-    return {}
+    console.log(packageA)
+    console.log(packageB)
+    for (const key in data) {
+        console.log(key)
+        //console.log(data[key])
+        data[key].versions.forEach(item => console.log(item))
+    }
 }
 
 exports.getLastCompatibleDependencies = getLastCompatibleDependencies;
