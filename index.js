a5b947019128fa9ff130cb3b7e91fb2cc88f0fdb
Alexander <bykovskiyan@gmail.com>
2023-07-10 08:23:40 +0400

    Variant 1

--- index.js
+++ index.js
@@ -5,21 +5,20 @@ let nailsCount;
 let coordinats = [];
 
 
-// const rl = readline.createInterface({
-//     input: process.stdin,
-//     output: process.stdout
-// });
+// Input data from keyboard
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-// Input data
-// rl.on('line', count => {
-//     rl.on('line', line => {
-//         nailsCount = Number(count);
-//         coordinats = stringToCoordinats(line)
-//         console.log(nailsCount, coordinats)
-//         //process.stdout.write(nailsCount, coordinats);
-//         rl.close();
-//     })
-// })
+rl.on('line', count => {
+    rl.on('line', line => {
+        nailsCount = Number(count);
+        coordinats = stringToCoordinats(line)
+        process.stdout.write(String(findTheLength(coordinats, nailsCount)));
+        rl.close();
+    })
+})
 
 
 // Convert input string to points with coordinats
@@ -30,8 +29,30 @@ function stringToCoordinats (inputString) {
 }
 
 
+// Main function
+function findTheLength (coordinats, nailsCount){
 
-const test = {
+    if(nailsCount === 0 || coordinats.length === 0) return 0;
+
+    const dp = Array(nailsCount);
+    dp.fill(0);
+    dp[1] = coordinats[1] - coordinats[0];
+
+    if (nailsCount > 2) {
+        dp[2] = coordinats[2] - coordinats[0];
+
+        for (let i = 3; i <= nailsCount; i++) {
+            dp[i] = Math.min(dp[i - 2], dp[i - 1]) + coordinats[i] - coordinats[i - 1]
+        }
+
+    }
+
+    return dp[nailsCount - 1]
+}
+
+
+// Test data (no used input from keyboard)
+/*const test = {
     count: '6',
     string: '3 13 12 4 14 6',
 }
@@ -39,4 +60,4 @@ const test = {
 nailsCount = Number(test.count);
 coordinats = stringToCoordinats(test.string);
 
-console.log(coordinats);
+console.log(findTheLength(coordinats, nailsCount));*/
