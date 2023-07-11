9876aadc35d071554b97f28e50a14d8ed3bbe80a
Alexander <bykovskiyan@gmail.com>
2023-07-11 10:46:25 +0400

    Temp commit

--- index.js
+++ index.js
@@ -1,1 +1,58 @@
-console.log('Hello World');
+const readline = require('readline');
+
+// Init variables
+let n,m;
+const board = [];
+
+// Input data from keyboard
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function readMatrix(n, m) {
+
+    function readBoardLine() {
+        rl.question(`Введите строку чисел из ${m} (через пробел): `, (line) => {
+            const numbers = line.split(' ').map(Number);
+            if (numbers.length === m) {
+                board.push(numbers);
+            } else {
+                console.log('Некорректные данные');
+            }
+
+            if (board.length < n) {
+                readBoardLine();
+            } else {
+                rl.close();
+
+                console.log('Вы ввели следующую матрицу:');
+                console.log(board);
+            }
+        });
+    }
+
+    readBoardLine();
+}
+
+
+function readNM() {
+
+    function readNMLine() {
+        rl.question('Введите N количество строк и M - столбцов (через пробел): ', (input) => {
+            [n, m] = input.split(' ').map(Number);
+            if (0 < n && n <= 20 && 0 < m && m <= 20) {
+                rl.close();
+                readMatrix(n, m);
+            } else {
+                console.log('Некорректные данные');
+                readNMLine();
+            }
+        });
+    }
+
+    readNMLine();
+}
+
+readNM();
+
